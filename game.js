df38046a47726be1308a203c3430ac77893ea68e
Max Woodcock <maxggwoodcock@gmail.com>
2023-02-23 08:32:36 +0800

    And even more shrink

--- game.js
+++ game.js
@@ -74,7 +74,7 @@ async function getPuzzleNum(data) {
 	ratingLower = parseInt(localStorage.getItem('playerRating')) - 100;
 	ratingUpper = parseInt(localStorage.getItem('playerRating')) + 100;
 	min = 0;
-	max = 46188;
+	max = 5000;
 	numFound = false;
 	while(numFound == false) { 
 		randomNum = Math.floor(Math.random() * (max - min) + min);
